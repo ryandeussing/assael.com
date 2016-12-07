03aa3265077703f34ba85649ac4f66bbb3574804
ryandeussing <ryan@ryandeussing.com>
2016-12-07 16:55:09 -0500

    lazyloading images when they are past 4 in the loop

--- _assets/js/global/main.js
+++ _assets/js/global/main.js
@@ -1,10 +1,13 @@
-(function() {
 
-  // $('.body-inner').fadeIn(2000);
+$(function() {
+  // fade in content
+  $('.body-inner').fadeIn(2000);
   setTimeout(function(){
     $('#body-inner').fadeIn();
   }, 250);
+});
 
+$(function() {
   // fonts
   (function(d) {
     var config = {
@@ -14,46 +17,10 @@
     },
     h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
   })(document);
+});
 
-  // visual grid: use 'g' key to toggle body class
-  // var k = new Kibo();
-  // k.down(['g'], function() {
-  //   $('body').addClass('grid');
-  // }).up('g', function() {
-  //   $('body').removeClass('grid');
-  // });
-
-  // enquire: media queries, but for javascript
-  // enquire.register("screen and (max-width: 48em)", {
-  //   match : function() {
-  //     console.log('up to 768px wide')
-  //   },
-  //   unmatch : function() {
-  //   }
-  // })
-  // .register("screen and (min-width: 48.1em)", {
-  //   match : function() {
-  //     console.log('over 768px wide')
-  //   },
-  //   unmatch : function() {
-  //   }
-  // });
-
-  // header navbar
-  // var menuToggle = $('#js-navigation-mobile-menu').unbind();
-  // $('#js-centered-navigation-menu').removeClass("show");
-
-  // menuToggle.on('click', function(e) {
-  //   e.preventDefault();
-  //   $('#js-centered-navigation-menu').slideToggle(function(){
-  //     if($('#js-centered-navigation-menu').is(':hidden')) {
-  //       $('#js-centered-navigation-menu').removeAttr('style');
-  //     }
-  //   });
-  // });
-
+$(function() {
   // menu toggle
-  setTimeout(function(){
     var menuToggle = $('#js-centered-navigation-mobile-menu').unbind();
     $('#js-centered-navigation-menu').removeClass("show");
 
@@ -67,6 +34,12 @@
         }
       });
     });
-  }, 250);
+});
 
-})();
+$(function() {
+  $('img.lazy').unveil({
+    throttle: 200,
+    placeholder: '/assets/img/trans.gif'
+  });
+});
+
