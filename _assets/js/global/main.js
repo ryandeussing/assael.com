63bfcaff8b7247b0e90874cf8495452cecab5656
ryandeussing <ryan@ryandeussing.com>
2016-12-07 15:39:26 -0500

    put menu-toggle in timeout so it works - guessing b/c no error

--- _assets/js/global/main.js
+++ _assets/js/global/main.js
@@ -3,11 +3,6 @@
   // $('.body-inner').fadeIn(2000);
   setTimeout(function(){
     $('#body-inner').fadeIn();
-    // $('img').unveil({
-        // offset: 100,
-        // throttle: 200
-        // placeholder: 'http://placehold.it/500x300'
-    // });
   }, 250);
 
   // fonts
@@ -20,28 +15,13 @@
     h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
   })(document);
 
-  // menu toggle
-  var menuToggle = $('#js-centered-navigation-mobile-menu').unbind();
-  $('#js-centered-navigation-menu').removeClass("show");
-
-  var dropDown = $('.nav-link.more');
-
-  menuToggle.on('click', function(e) {
-    e.preventDefault();
-    $('#js-centered-navigation-menu').slideToggle(function(){
-      if($('#js-centered-navigation-menu').is(':hidden')) {
-        $('#js-centered-navigation-menu').removeAttr('style');
-      }
-    });
-  });
-
   // visual grid: use 'g' key to toggle body class
-  var k = new Kibo();
-  k.down(['g'], function() {
-    $('body').addClass('grid');
-  }).up('g', function() {
-    $('body').removeClass('grid');
-  });
+  // var k = new Kibo();
+  // k.down(['g'], function() {
+  //   $('body').addClass('grid');
+  // }).up('g', function() {
+  //   $('body').removeClass('grid');
+  // });
 
   // enquire: media queries, but for javascript
   // enquire.register("screen and (max-width: 48em)", {
@@ -71,5 +51,22 @@
   //     }
   //   });
   // });
+
+  // menu toggle
+  setTimeout(function(){
+    var menuToggle = $('#js-centered-navigation-mobile-menu').unbind();
+    $('#js-centered-navigation-menu').removeClass("show");
+
+    var dropDown = $('.nav-link.more');
+
+    menuToggle.on('click', function(e) {
+      e.preventDefault();
+      $('#js-centered-navigation-menu').slideToggle(function(){
+        if($('#js-centered-navigation-menu').is(':hidden')) {
+          $('#js-centered-navigation-menu').removeAttr('style');
+        }
+      });
+    });
+  }, 250);
 
 })();
