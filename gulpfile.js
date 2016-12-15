f5cf8e4a3e46440452184d3ef9ca18b0e5972a3e
ryandeussing <ryan@ryandeussing.com>
2016-12-15 15:23:56 -0500

    comment on imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -88,6 +88,7 @@ gulp.task('build:scripts', ['build:scripts:global']);
 gulp.task('build:images', function() {
     return gulp.src(paths.imageFilesGlob)
         // TODO: set this up to only happen on new images
+        //  remove it for now
         // .pipe(imagemin())
         .pipe(gulp.dest(paths.jekyllImageFiles))
         .pipe(gulp.dest(paths.siteImageFiles))
